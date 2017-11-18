ff73558f35d3ae0c5d080c3d1f74d334c8aadd86
Christos Topaloudis <topless@gmail.com>
2017-11-18 18:06:45 +0100

    Bootstrap for style and vue-strap for interactions

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -2,6 +2,11 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
+    <alert :show.sync="show" placement="top-right" type="success" width="400px" dismissable>
+      <span class="icon-ok-circled alert-icon-float-left"></span>
+      <strong>Well Done!</strong>
+      <p>You successfully read this important alert message.</p>
+    </alert>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
@@ -22,11 +27,15 @@
 
 <script>
 import axios from 'axios'
+import { alert } from 'vue-strap'
+
 export default {
   name: 'HelloWorld',
+  components: { alert },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App with gae-init'
+      msg: 'Welcome to Your Vue.js App with gae-init',
+      show: true
     }
   },
 
@@ -41,24 +50,3 @@ export default {
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
